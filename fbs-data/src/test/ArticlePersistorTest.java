3da1427cf9d87ac7ef9890e4b5a99431e0d13c0c
Pascal <pascal.stalder@stud.hslu.ch>
2018-04-04 22:11:56 +0200

    hibernate try

---
+++ fbs-data/src/test/ArticlePersistorTest.java
@@ -0,0 +1,14 @@
+package ch.hslu.appe.fbs.data;
+
+import static org.junit.Assert.*;
+
+public class ArticlePersistorTest {
+
+    @org.junit.Test
+    public void getById() {
+    }
+
+    @org.junit.Test
+    public void getList() {
+    }
+}
