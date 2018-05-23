1b2e16e09589d9ddbd1111e63d3e7fa69146afbb
luca_ <luca.paivadias@stud.hslu.ch>
2018-05-23 14:23:51 +0200

    changed function name in ArticlePersistor from delete to deleteByArticleNr

--- fbs-data/src/main/java/ch/hslu/appe/fbs/data/ArticlePersistor.java
+++ fbs-data/src/main/java/ch/hslu/appe/fbs/data/ArticlePersistor.java
@@ -97,7 +97,7 @@ public final class ArticlePersistor {
         return article;
     }
 
-    public FBSFeedback delete(Article article) {
+    public FBSFeedback deleteByArticleNr(Article article) {
 
         try {
             Util.transactionBegin();
